0fb689657e14ee7d3347dc66b39c26bc7ee80497
CARLINA18 <ana.carolina.aristizabal@gmail.com>
2021-05-16 18:27:01 -0500

    archivos en go

---
+++ hola.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func main(){
+	var nombre  string
+	var  edad int
+	nombre = "ANA"
+	edad = 26
+	peso := 60
+	fmt.Println("HOLA MUNDO",nombre ,edad)
+	fmt.Println(edad+peso)
+}
